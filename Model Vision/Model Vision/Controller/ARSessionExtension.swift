020056c9a5ea29cca687ac0bf7b53e4d0bb9b876
Pranav Jain <pranajain@gmail.com>
2019-02-04 16:00:17 -0500

    Switched orientation and angle velocity to be in terms of degrees instead of radians

--- Model Vision/Model Vision/Controller/ARSessionExtension.swift
+++ Model Vision/Model Vision/Controller/ARSessionExtension.swift
@@ -38,28 +38,44 @@ extension ViewController: ARSessionDelegate {
                 if let goalNode = self.goalNode {
                     
                     //update data
-                    pilotBrain.droneLocation = droneNode.worldPosition
-                    pilotBrain.goalLocation = goalNode.worldPosition
-                    pilotBrain.droneRotation = droneNode.eulerAngles
+                    //double check this
+                    //pilotBrain.droneLocation = droneNode.worldPosition
+                    pilotBrain.droneLocation = SCNVector3Zero
+                    let goalLoc = droneNode.convertPosition(goalNode.worldPosition, from: self.sceneView.scene.rootNode)
+                    pilotBrain.goalLocation = goalLoc
+                    let eulerAnglesInDegrees = droneNode.eulerAngles * (180.0/Float.pi)
+                    pilotBrain.droneRotation = eulerAnglesInDegrees
                     pilotBrain.droneVelocity = droneNode.physicsBody!.velocity
-                    //droneNode.physicsBody!
+                    let angularVelocity = droneNode.physicsBody!.angularVelocity
+                    let angularMagnitudeInDeg = angularVelocity.w * (180.0/Float.pi)
+                    pilotBrain.droneAngleVelocity = SCNVector3(angularVelocity.x, angularVelocity.y, angularVelocity.z)*angularMagnitudeInDeg
                     //droneNode.physicsBody!.angularVelocity
-                    pilotBrain.droneAngleVelocity = droneNode.physicsBody!.angularVelocityFactor
-                    //droneNode.physicsBody!.angularVelocity
-                    pilotBrain.droneAcceleration = SCNVector3Zero
+                    
                     
                     let newForces = pilotBrain.getDroneVectors()
-                    print(newForces)
+                    //pilotBrain.droneAcceleration = SCNVector3(x: 0.0, y: -9.8, z: 0.0)
                     droneNode.physicsBody!.clearAllForces()
+                    print("------------")
+                    print(newForces)
                     for i in 0...newForces.count-1 {
                         let force = newForces[i]
                         
-                        let finalForce = self.clamp( Double(exactly: force)! * 20.0, minValue: 0.0, maxValue: 20.0)
+                        let finalForce = self.clamp( Double(exactly: force)! * 40.0, minValue: 0.0, maxValue: 40.0)
+                        //print(finalForce)
+                        /*for thruster in pilotBrain.thrusters {
+                            if i%2 == 0 {
+                                let forceVector = SCNVector3(0, finalForce, 0)
+                                droneNode.physicsBody!.applyForce(forceVector, at: thruster.position, asImpulse: false)
+                                //pilotBrain.droneAcceleration += forceVector
+                            }
+                        }*/
                         if i%2 == 0 {
+                            print(finalForce)
                             let thruster = pilotBrain.thrusters[i]
+                            //droneNode.rotation
                             let forceVector = SCNVector3(0, finalForce, 0)
                             droneNode.physicsBody!.applyForce(forceVector, at: thruster.position, asImpulse: false)
-                            
+                            //pilotBrain.droneAcceleration += forceVector
                         }
                         
                     }
